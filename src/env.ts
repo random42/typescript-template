896b15ec39bf69775644843a5aa44342c23d317d
Roberto Sero <>
2025-05-08 01:58:57 +0200

    fix: set default value for NODE_ENV in environment configuration

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,9 @@ import { cleanEnv, num, str } from 'envalid';
 
 dotenv.config();
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ['development', 'production'] }),
+  NODE_ENV: str({
+    choices: ['development', 'production'],
+    default: 'production',
+  }),
   LOG_LEVEL: str({ default: 'info' }),
 });
